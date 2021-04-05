38a6016366b9b450bf9b37921170c996bf93c4f0
wesbin <leegosoo11@naver.com>
2021-04-05 15:28:04 +0900

    - emoji update

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,36 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+              // In your gatsby-transformer-remark plugin array
+              plugins: [{
+                resolve: 'gatsby-remark-emojis',
+                options: {
+                  // Deactivate the plugin globally (default: true)
+                  active : true,
+                  // Add a custom css class
+                  class  : 'emoji-icon',
+                  // In order to avoid pattern mismatch you can specify
+                  // an escape character which will be prepended to the
+                  // actual pattern (e.g. `#:poop:`).
+                  escapeCharacter : '#', // (default: '')
+                  // Select the size (available size: 16, 24, 32, 64)
+                  size   : 64,
+                  // Add custom styles
+                  styles : {
+                    display      : 'inline',
+                    margin       : '0',
+                    'margin-top' : '1px',
+                    position     : 'relative',
+                    top          : '5px',
+                    width        : '25px'
+                  }
+                }
+              }]
+            }
+          }
         ],
       },
     },
@@ -140,6 +170,6 @@ module.exports = {
           }
         ]
       }
-    }
+    },
   ],
 }
