8241910e0b55012063d2b8dfaf8cfc2bb1cefd47
wesbin <leegosoo11@naver.com>
2021-05-31 10:58:23 +0900

    크롤링 => 스크래핑

--- src/templates/blog-about.js
+++ src/templates/blog-about.js
@@ -156,9 +156,9 @@ const BlogAbout = ({ data, location }) => {
                             className={classes.projects}>
                         <Typography className={classes.projectTitle}
                                     variant={'h5'}>
-                          우분투 패키지 크롤링(ubuntu-package-crawling)
+                          우분투 패키지 크롤링(ubuntu-package-scraping)
                         </Typography>
-                        <Button href="https://github.com/wesbin/ubuntu-package-crawling" target="_blank" rel="noopener noreferrer">
+                        <Button href="https://github.com/wesbin/ubuntu-package-scraping" target="_blank" rel="noopener noreferrer">
                           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="" title="github">
                             <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                           </svg>
@@ -166,7 +166,7 @@ const BlogAbout = ({ data, location }) => {
                         <Grid item
                               className={classes.projectsBody}>
                           <Typography>
-                            업무 중에 우분투 패키지를 반복해서 다운로드받아야 할 상황이 생겨 만든 툴입니다.
+                            우분투 패키지를 반복해서 다운로드받아야 할 상황이 생겨 만든 툴입니다.
                             <br/>
                             <br/>
                             Node.js를 이용해 구현하였습니다.
